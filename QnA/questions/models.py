d9ca9cd933e3de0280fae0516143c6b98e1bcd86
Hari <hari@admins-MacBook-Pro.local>
2021-10-05 22:49:26 +0530

    Create models in the questions app

--- QnA/questions/models.py
+++ QnA/questions/models.py
@@ -1,6 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from typing import Text
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import (
+    GenericForeignKey,
+    GenericRelation,
+)
 
-# Create your models here.
+from taggit.managers import TaggableManager
+
+
+class CreationDetail(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
+
+
+class Comment(CreationDetail, models.Model):
+    text = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    target = GenericForeignKey()
+
+
+class Question(CreationDetail, models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    tags = TaggableManager()
+    comments = GenericRelation(Comment)
+
+
+class Answer(CreationDetail, models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="answers"
+    )
+    text = models.TextField()
+    comments = GenericRelation(Comment)
