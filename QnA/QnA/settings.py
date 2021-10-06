f8b8d85b334edce84aeb649f2e0e9d007d07a849
Hari <hari@admins-MacBook-Pro.local>
2021-10-06 15:07:34 +0530

    Create base template

--- QnA/QnA/settings.py
+++ QnA/QnA/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "taggit",
     "vote",
     "questions",
+    "tailwind",
+    "theme",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = "QnA.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -121,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+TAILWIND_APP_NAME = "theme"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
