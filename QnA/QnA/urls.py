4be9b6027ae2a940398e804da9f4e7f6f6817925
Hari <hari@admins-MacBook-Pro.local>
2021-10-06 15:30:26 +0530

    Create urls.py

--- QnA/QnA/urls.py
+++ QnA/QnA/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from questions import views
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("", views.index, name="home"),
 ]
