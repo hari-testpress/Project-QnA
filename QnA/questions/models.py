4c278061ab727614741abc6d9ab01f9859b417af
Hari <hari@admins-MacBook-Pro.local>
2021-10-07 20:31:59 +0530

    Create Question, Answer, and Comment model

--- QnA/questions/models.py
+++ QnA/questions/models.py
@@ -1,6 +1,33 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import (
+    GenericForeignKey,
+    GenericRelation,
+)
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+
+
+class Comment(TimeStampedModel, models.Model):
+    text = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    target = GenericForeignKey()
+
+
+class Question(TimeStampedModel):
+    title = models.CharField(max_length=90)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = GenericRelation(Comment)
+
+
+class Answer(TimeStampedModel):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="answers"
+    )
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    comments = GenericRelation(Comment)
