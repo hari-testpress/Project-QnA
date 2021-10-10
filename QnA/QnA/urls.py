f30e56fab32397a4ec603ec1df3ae5bc3a4a8739
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-10-10 13:00:16 +0530

    Create base template (#4)
    * Create base template

--- QnA/QnA/urls.py
+++ QnA/QnA/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from questions import views
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
+    path("", views.index, name="home"),
 ]
