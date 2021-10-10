4c945d6cea088c91545df787b6af936938ea61ef
hari-testpress <hari@testpress.in>
2021-10-10 17:34:34 +0530

    fix the bug

--- QnA/questions/views.py
+++ QnA/questions/views.py
@@ -45,5 +45,4 @@ class QuestionCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.created_by = self.request.user
-        self.object.save()
         return super().form_valid(form)
