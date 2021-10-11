43aced6e2c7255d5d14c85f58d5c7b7cae85e63f
hari-testpress <hari@testpress.in>
2021-10-11 10:49:32 +0530

    fFix the bug in the unit test

--- QnA/tests/questions/test_view_question_list.py
+++ QnA/tests/questions/test_view_question_list.py
@@ -3,12 +3,22 @@ from django.urls import reverse, resolve
 from django.test import TestCase
 
 from questions.views import QuestionListView
+from questions.models import Question
 
 
 class QuestionListViewTests(TestCase):
     def setUp(self):
         self.url = reverse("home")
         self.response = self.client.get(self.url)
+        self.user = User.objects.create_user(username="hari", password="1234")
+        self.client.login(username="hari", password="1234")
+        for i in range(15):
+            question = Question(
+                title=f"question{i}",
+                description=f"description{i}",
+                created_by=self.user,
+            )
+            question.save()
 
     def test_status_code_is_200(self):
         self.assertEquals(self.response.status_code, 200)
