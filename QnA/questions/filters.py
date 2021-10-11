4aa0789b94027600ee2b903f0a99780e854009c6
hari-testpress <hari@testpress.in>
2021-10-11 14:03:57 +0530

    Remove the dead code

--- QnA/questions/filters.py
+++ QnA/questions/filters.py
@@ -6,10 +6,6 @@ from taggit.forms import TagField
 class TagFilter(django_filters.CharFilter):
     field_class = TagField
 
-    # def __init__(self, *args, **kwargs):
-    #     kwargs.setdefault("lookup_expr", "in")
-    #     super().__init__(*args, **kwargs)
-
 
 class QuestionFilter(django_filters.FilterSet):
     tags = TagFilter(field_name="tags__name", lookup_expr="in")
