f3353cd6801faa20873ca2b87d3c09dda66ba7ed
hari-testpress <hari@testpress.in>
2021-10-11 14:37:44 +0530

    Refactor the test case

--- QnA/tests/questions/test_view_create_comment.py
+++ QnA/tests/questions/test_view_create_comment.py
@@ -5,22 +5,7 @@ from django.test import TestCase
 from questions.models import Question, Answer, Comment
 
 
-class PostCommentTestMixin(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username="hari", password="1234")
-        self.client.login(username="hari", password="1234")
-        self.question = Question(
-            title="title", description="description", created_by=self.user
-        )
-        self.question.save()
-        self.url = reverse(
-            "questions:post-comment-on-question", args=[self.question.id]
-        )
-        self.response = self.client.get(self.url)
-        self.success_url = reverse(
-            "questions:question_detail", args=[self.question.id]
-        )
-
+class PostCommentTestMixin:
     def test_view_stores_the_comment(self):
         self.client.post(self.url, {"text": "comment"})
         self.assertTrue(Comment.objects.exists())
@@ -30,8 +15,12 @@ class PostCommentTestMixin(TestCase):
         response = self.client.post(self.url, {})
         self.assertRedirects(response, f"/accounts/login/?next={self.url}")
 
+    def test_valid_form_submission_redirect_to_the_detail_page(self):
+        response = self.client.post(self.url, {"text": "comment"})
+        self.assertRedirects(response, self.success_url)
 
-class PostCommentOnAnswer(TestCase):
+
+class PostCommentOnAnswer(PostCommentTestMixin, TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="hari", password="1234")
         self.client.login(username="hari", password="1234")
@@ -53,11 +42,19 @@ class PostCommentOnAnswer(TestCase):
             "questions:question_detail", args=[self.question.id]
         )
 
-    def test_view_stores_the_comment(self):
-        self.client.post(self.url, {"text": "comment"})
-        self.assertTrue(Comment.objects.exists())
 
-    def test_unauthorized_user_redirected_to_the_login_page(self):
-        self.client.logout()
-        response = self.client.get(self.url)
-        self.assertRedirects(response, f"/accounts/login/?next={self.url}")
+class PostCommentOnQuestion(PostCommentTestMixin, TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="hari", password="1234")
+        self.client.login(username="hari", password="1234")
+        self.question = Question(
+            title="title", description="description", created_by=self.user
+        )
+        self.question.save()
+        self.url = reverse(
+            "questions:post-comment-on-question", args=[self.question.id]
+        )
+        self.response = self.client.get(self.url)
+        self.success_url = reverse(
+            "questions:question_detail", args=[self.question.id]
+        )
